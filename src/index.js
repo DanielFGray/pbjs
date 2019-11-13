ca0f1b22ebd7f3a1c74f968e4dd864cdd3653419
DanielFGray <DanielFGray@gmail.com>
2019-11-13 12:39:51 -0600

    stay fresh and use multipart

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const Koa = require('koa')
+const helmet = require('koa-helmet')
 const kbody = require('koa-body')
 const router = require('./router')
 
@@ -14,7 +15,13 @@ if (! PORT) throw new Error('must define a PORT env variable')
 
 const app = new Koa()
 
-app.use(kbody())
+app.use(helmet())
+app.use(kbody({
+  multipart: true,
+  textLimit: '4mb',
+  jsonLimit: '4mb',
+  formLimit: '4mb',
+}))
 app.use(router)
 
 app.listen(PORT, HOST, () => {
--- src/router.js
+++ src/router.js
@@ -5,12 +5,20 @@ const db = require('./db')
 
 const router = new Router()
 
+router.get('/', async ctx => {
+  ctx.body = `to upload a new paste:\n  your command | curl -F 'body=<-' ${process.env.APP_PATH}/`
+})
+
 router.get('/:id', async ctx => {
   const { id } = ctx.params
   try {
-    const [{ body }] = await db('pastes').select('body').where({ id })
+    const res = await db('pastes').select('body').where({ id })
+    const body = res && res[0] && res[0].body
     if (body) {
       ctx.body = body
+    } else {
+      ctx.body = 'Not Found\n'
+      ctx.status = 404
     }
   } catch (e) {
     ctx.body = e.message
@@ -21,12 +29,14 @@ router.post('/', async ctx => {
   const { body } = ctx.request.body
   if (!body) {
     ctx.body = 'Missing "body" field'
+    console.log(ctx.request)
     return
   }
   try {
     const id = uuid()
     await db('pastes').insert({ body, id })
-    ctx.body = id
+    ctx.body = `${process.env.APP_PATH}/${id}\n`
+    console.log(ctx.request)
   } catch (e) {
     ctx.body = e.message
   }
